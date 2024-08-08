0d1a0a7bb56348a5eebfce38716a3a7b6fc10b0c
shivam2399 <dubey.shivam2399@gmail.com>
2024-08-08 23:12:11 +0530

    Links for dynamic routes

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,7 +1,23 @@
+import Link from "next/link";
+
 const AboutUs = () => {
+    const details = [
+        { id: 1, name: 'Yash', role: 'Senior Developer' },
+        { id: 2, name: 'Vaibhav', role: 'Backend Developer' },
+        { id: 3, name: 'Suresh', role: 'Frontend Developer' }
+    ]
     return (
         <>
-         <h1>About Us</h1>
+         <h1>Developers List</h1>
+         <ul>
+            {details.map(dev => (
+                <li key={dev.id}>
+                    <Link href={`/aboutus/${dev.id}`}>
+                      {dev.name}
+                    </Link>
+                </li>
+            ))}
+         </ul>
         </>
     )
 }
